55f77aef31a4944376ab6cbaff071ac72b1422b7
Nora <anoor@witweel.com>
2022-01-25 19:27:59 -0500

    test

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_PudCxymHK', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '15go1i5hiflbi3vg4695he818b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
