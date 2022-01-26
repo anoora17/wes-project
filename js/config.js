24843cafe8742780cdd0bf198dd42b069bb793bd
Nora <anoor@witweel.com>
2022-01-25 19:46:38 -0500

    test

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'us-west-2_PudCxymHK', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '15go1i5hiflbi3vg4695he818b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://bzvibzfy51.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
